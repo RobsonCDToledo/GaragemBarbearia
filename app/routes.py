4e6343443fb1c856fd8bd152d87b4f362aaf7121
RobsonCDToledo <robson.rotta@gmail.com>
2022-11-06 01:49:39 -0300

    ajustando rota

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import Flask,  render_template
 
 @app.route('/')
 @app.route('/index')
@@ -7,7 +7,7 @@ from flask import render_template
 def index():
     nome = 'Luciano'
     dados = {'profissao': 'Barbeiro','endereco': 'Avenida Pedro Luiz da Costa, 155/1 - Costa Rios', 'cidade': 'Pouso Alegre-MG'}
-    return render_template('Index.html', nome=nome, dados=dados)
+    return render_template('index.html', nome=nome, dados=dados)
 
 @app.route('/contato')
 def contato():
