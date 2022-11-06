5419f969b66453c5ff959acc07bca73f13fc9ffd
RobsonCDToledo <robson.rotta@gmail.com>
2022-11-06 01:19:12 -0300

    first commit

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=garagembarbearia.py
+FLASK_ENV=development
---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    nome = 'Luciano'
+    dados = {'profissao': 'Barbeiro','endereco': 'Avenida Pedro Luiz da Costa, 155/1 - Costa Rios', 'cidade': 'Pouso Alegre-MG'}
+    return render_template('Index.html', nome=nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+ 
